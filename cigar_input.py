1f66fa493c9cb6567fe8606890595927aded458c
JRMeyer <joshua.richard.meyer@gmail.com>
2015-12-28 13:56:34 -0700

    now the main function takes in a path to either a dir or a file (image), and returns a numpy array of the image data

--- cigar_input.py
+++ cigar_input.py
@@ -1,53 +1,63 @@
 import tensorflow.python.platform
 import tensorflow as tf
 import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
 
-def read_cigar(dir):
-  """Reads and parses examples from our cigar data files.
-  Recommendation: if you want N-way read parallelism, call this function
-  N times.  This will give you N independent Readers reading different
-  files & positions within those files, which will give better mixing of
-  examples.
-  Args:
-    dir: Directory to read in the files
-  Returns:
-    An object representing a single example, with the following fields:
-      height: number of rows in the result (128)
-      width: number of columns in the result (128)
-      depth: number of color channels in the result (3)
-      key: a scalar string Tensor describing the filename & record number
-        for this example.
-      label: an int32 Tensor with the label in the range 0..9.
-      uint8image: a [height, width, depth] uint8 Tensor with the image data
-  """
-  class CigarRecord(object):
-    pass
-  result = CigarRecord()
-  # Dimensions of the images in the dataset.
-  label_bytes = 1  # We only have 4 labels
-  result.height = 128
-  result.width = 128
-  result.depth = 3
-  image_bytes = result.height * result.width * result.depth
-  # Get all files to read from current directory
-  filenames = os.listdir(dir)
-  # Create a queue that produces the filenames to read.
+def read_cigar(path):
+  '''
+  imput:
+    path: a path to a dir or image file
+  
+  returns:
+    images: a numpy array of arrays of images
+  '''
+  # Create a queue that produces the filenames to read
+  if os.path.isdir(path):
+    filenames = [os.path.join(path,filename) for filename in os.listdir(path)]
+  elif os.path.isfile(path):
+    filenames= [path]
   filename_queue = tf.train.string_input_producer(filenames)
-  # Every record consists of a label followed by the image, with a
-  # fixed number of bytes for each.
-  record_bytes = label_bytes + image_bytes
-  # Read a record, getting filenames from the filename_queue.
-  reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
-  result.key, value = reader.read(filename_queue)
-  # Convert from a string to a vector of uint8 that is record_bytes long.
-  record_bytes = tf.decode_raw(value, tf.uint8)
-  # The first bytes represent the label, which we convert from uint8->int32.
-  result.label = tf.cast(
-      tf.slice(record_bytes, [0], [label_bytes]), tf.int32)
-  # The remaining bytes after the label represent the image, which we reshape
-  # from [depth * height * width] to [depth, height, width].
-  depth_major = tf.reshape(tf.slice(record_bytes, [label_bytes], [image_bytes]),
-                           [result.depth, result.height, result.width])
-  # Convert from [depth, height, width] to [height, width, depth].
-  result.uint8image = tf.transpose(depth_major, [1, 2, 0])
-  return result
+
+  # our tensorflow ops read images, getting filenames from the filename_queue
+  reader = tf.WholeFileReader()
+  key, value = reader.read(filename_queue)
+  img = tf.image.decode_jpeg(value)
+  init_op = tf.initialize_all_variables()
+
+  # initialize ops and run the session
+  with tf.Session() as sess:
+    sess.run(init_op)
+    # start populating the filename queue
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    images=[]
+    for i in range(len(filenames)):
+      image = img.eval()
+      print('image.shape '+ str(image.shape))
+      plot_image(image)
+      images.append(image)
+    coord.request_stop()
+    coord.join(threads)
+
+  images = np.asarray(images)
+  print('total images read: ' + str(len(images)))
+  return(images)
+
+def plot_image(image):
+  '''
+  given an image as an array, plot it if it's RGB
+  '''
+  if image.shape[2] == 3:
+    fig = plt.figure()
+    plt.imshow(image)
+    plt.show()
+  elif image.shape[2] == 1:
+    print('GRAYSCALE IMAGE... dont know how to show')
+
+if __name__ == '__main__':
+  import sys
+  path = sys.argv[1]
+
+  imageData = read_cigar(path)
