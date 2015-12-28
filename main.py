20c02eb3a8ed3c3672e98b05fcd92991574860a0
Joe Meyer <josephedwardmeyer@gmail.com>
2015-12-28 17:17:46 -0600

    62% GET AT US

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-# # Import data from mnist to get images
-# from tensorflow.examples.tutorials.mnist import input_data
-# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
 import tensorflow as tf
+import cigar_input as ci
+import sys, random
+
+def get_train_data(batchSize):
+  indexes = range(features.shape[0])
+  indexes = [i for i in indexes]
+  random.shuffle(indexes)
+  
+  indexes = indexes[:batchSize]
+
+  return features[indexes,:], labels[indexes,:]
+  
+path = sys.argv[1]
+
+labels, features = ci.main(path)
 
 # Define our tensor to hold images. 16384 = pixels, None indicates
 # we can hold any number of images
@@ -32,11 +43,11 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-# Train. In this case, 1000 times
-for i in range(1000):
+# Train. In this case, 500 times
+for i in range(500):
   # Feeds in 100 random images each time for training (stochastic training)
   # batch_xs are our images (pixels), batch_ys are our correct outputs
-  batch_xs, batch_ys = mnist.train.next_batch(100)
+  batch_xs, batch_ys = get_train_data(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
   
 # tf.argmax gives the index of the highest entry in a tensor along some axis
@@ -48,8 +59,10 @@ correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 # False is 0 and True is 1, what was our average?
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
+testX, testY = get_train_data(50)
+
 # How well did we do?
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print(sess.run(accuracy, feed_dict={x: testX, y_: testY}))
 
 # Close the session (self documenting code)
 sess.close()
