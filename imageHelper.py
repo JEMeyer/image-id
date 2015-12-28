502e1d8b313fc3ad2e7b65b3942ac7e83d070d7a
JRMeyer <joshua.richard.meyer@gmail.com>
2015-12-27 21:31:16 -0700

    now all the conversions are instantiated as functions which take an image or path to an image as an argument and return a converted image

--- imageHelper.py
+++ imageHelper.py
@@ -1,63 +1,69 @@
 import os, sys, glob
 from PIL import Image
 
-def resize_images_in_dir(dir, size = (128,128)):
-    '''
-    dir is directory of images
-    size (optional) is max pixel W and H of generated images
-    '''
-    for infile in glob.glob(dir + '*.jpg'):
-        file, ext = os.path.splitext(infile)
-        base = os.path.basename(file)
-        abspath = os.path.dirname(file)
-        img = Image.open(infile)
-        img.thumbnail(size, Image.ANTIALIAS)
-        img.save(os.path.join(abspath, '_resized_' + base + '.jpg'), 'JPEG')
-    
-    
-def paste_on_background(dir, size = (128,128)):
-    ''' 
-    dir is directory of foreground images
-    size (optional) is an integer tuple for background size
-    '''
-    for infile in glob.glob(dir + '*.jpg'):
-        file, ext = os.path.splitext(infile)
-        base = os.path.basename(file)
-        abspath = os.path.dirname(file)
 
-        img = Image.open(infile) 
-        img_w, img_h = img.size
-        # (255,255,255) = white
-        background = Image.new('RGB', size)
-        bg_w, bg_h = background.size
-        offset = (int((bg_w - img_w) / 2), int((bg_h - img_h) / 2))
-        background.paste(img, offset)
-        background.save(os.path.join(abspath, '_normalized_' + base + '.jpg'), 
-                        'JPEG')
+def resize_img(imgFile,newSize):
+    '''
+    imgFile is the absolute path to an image, or an JpegImageFile
+    newSize is max pixel W and H of new image
+    '''
+    if isinstance(imgFile, str):
+        img = Image.open(imgFile)
+    else:
+        img = imgFile
+    img.thumbnail(newSize, Image.ANTIALIAS)
+    return img
 
 
-def convert_color(dir, color='gray'):
-    ''' 
-    dir is directory of images
+def paste_img_on_background(imgFile, backgroundSize):
+    '''
+    imgFile is the absolute path to an image, or an JpegImageFile
+    backgroundSize is the size of the background onto which
+    the image from imgFile is pasted
+    '''
+    if isinstance(imgFile, str):
+        img = Image.open(imgFile)
+    else:
+        img = imgFile
+    img_w, img_h = img.size
+    background = Image.new('RGB', backgroundSize)
+    bg_w, bg_h = background.size
+    offset = (int((bg_w - img_w) / 2), int((bg_h - img_h) / 2))
+    background.paste(img, offset)
+    return background
+
+
+def convert_color_of_img(imgFile, color):
+    '''
+    imgFile is the absolute path to an image, or an JpegImageFile
     color is either 'RGB' or 'gray' or 'bw'
     '''
+    if isinstance(imgFile, str):
+        img = Image.open(imgFile)
+    else:
+        img = imgFile
+    if color == 'gray':
+        img = img.convert('L')
+    elif color == 'RGB':
+        img = img.convert('RGB')
+    elif color == 'bw':
+        img = img.convert('1')
+    return img
+
+
+if __name__ == '__main__':
+    dir = sys.argv[1]
+
     for infile in glob.glob(dir + '*.jpg'):
+        # read in file and split path apart
         file, ext = os.path.splitext(infile)
         base = os.path.basename(file)
         abspath = os.path.dirname(file)
-        img = Image.open(infile)
-        if color == 'gray':
-            img = img.convert('L')
-        elif color == 'RGB':
-            img = img.convert('RGB')
-        elif color == 'bw':
-            img = img.convert('1')
-        img.save(os.path.join(abspath, '_color_' + base + '.jpg'), 'JPEG')
 
+        # do relevant tranformations on image
+        smallImg = resize_img(infile, newSize=(128,128))
+        squareImg = paste_img_on_background(smallImg,backgroundSize=(128,128))
+        newImg = convert_color_of_img(squareImg,'gray')
 
-
-
-
-
-
-
+        # save new image
+        newImg.save(os.path.join(abspath, '_new_' + base + '.jpg'), 'JPEG')
