1ced3396ab87df834b916ad9dff8de143a244870
Joe Meyer <josephedwardmeyer@gmail.com>
2016-01-02 17:26:41 -0600

    added plant_input.py for getting plant images to feed into neural net

---
+++ plant_input.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image
+import numpy as np
+import glob
+
+
+def input_data(path):
+  newPath = os.path.join(path,'*.jpg')
+  filePaths = glob.glob(newPath)
+  numImgs = len(filePaths)
+
+  labels = np.empty(shape = (numImgs,2))
+  features = np.empty(shape = (numImgs,16384))
+
+  for i,imagePath in enumerate(filePaths):
+    if 'pinoak' in imagePath:
+      labelVec = [1,0]
+    elif 'sugarmaple' in imagePath:
+      labelVec = [0,1]
+
+    # if we load one image at a time, this is much faster than load_images()
+    img = np.asarray(Image.open(imagePath)).ravel()
+
+    features[i,:] = img
+    labels[i,:] = labelVec
+  return features,labels
